6cf1e8ebb385e2f4213fa6ca38ba14fc0f806dfc
kent <kent.tong.mo@gmail.com>
2020-01-22 19:03:29 +0800

    implemented timer in js and imported as python

---
+++ src/lib/p4k/timer.js
@@ -0,0 +1,20 @@
+var $builtinmodule=function(name) {
+  var mod={};
+  mod.Timer=Sk.misceval.buildClass(mod, function($gbl, $loc) {
+    $loc.__init__=new Sk.builtin.func(function (self, intMs) {
+      var intv=Sk.ffi.remapToJs(intMs);
+      self.timerId=setInterval(function() {
+      	 Sk.misceval.callsimArray(self["on_timer"], [self]);
+      }, intv);
+      return Sk.builtin.none.none$;
+    });
+    $loc.cancel=new Sk.builtin.func(function (self) {
+      clearInterval(self.timerId);
+      return Sk.builtin.none.none$;
+    });
+    $loc.on_timer=new Sk.builtin.func(function (self) {
+      return Sk.builtin.none.none$;
+    });
+  }, 'Timer', []);
+  return mod;
+};
