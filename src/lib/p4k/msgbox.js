2a9ec4fd690e964b7220fda8765bdcda97833293
Kent Tong <kent.tong.mo@gmail.com>
2020-01-27 17:50:08 +0800

    dlgbox

--- src/lib/p4k/msgbox.js
+++ src/lib/p4k/msgbox.js
@@ -5,5 +5,16 @@ var $builtinmodule=function(name) {
     window.alert(msg);
     return Sk.builtin.none.none$;
   });
+  mod.show_dlg=new Sk.builtin.func(function (title, contentHtml, onCloseFunc) {
+    title=Sk.ffi.remapToJs(title);
+    contentHtml=Sk.ffi.remapToJs(contentHtml);
+    $(contentHtml).dialog({
+      title: title,
+      close: function() {
+        Sk.misceval.applyAsync(undefined, onCloseFunc, undefined, undefined, undefined, []);
+      }
+    });
+    return Sk.builtin.none.none$;
+  });
   return mod;
 };
